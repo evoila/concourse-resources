187b96e8c1397a477113542b4a0f19e6b149c458
Patrick Weber <pweber@evoila.de>
2019-06-26 10:28:53 +0200

    cleanup

--- concourse-credhub-resource/asserts/addJsonXML.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-INPUT="$(cat)"
-echo "$INPUT"
-path=`echo "$INPUT" | jq   -r  '.path'`
-echo "$path" | 	$RES/addPom.sh  "$1"
-echo "$INPUT" | jq   -r  '.insert' | $RES/parseJson.sh "$path" "$1" 
